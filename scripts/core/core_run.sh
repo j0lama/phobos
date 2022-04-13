1484407d7b94b32b443ec453b78fadac0e8919a1
j0lama <jon_larrea@hotmail.com>
2022-04-13 15:44:11 +0100

    Core scripts added

---
+++ scripts/core/core_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo /local/repository/open5gs/build/tests/app/epc -c /local/repository/config/config.yaml
---
+++ scripts/core/core_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Move to repository folder
+cd /local/repository
+
+# Install Mongo
+sudo apt update
+sudo apt install mongodb
+
+
+# Install dependencies
+sudo apt install python3-pip python3-setuptools python3-wheel ninja-build build-essential flex bison git libsctp-dev libgnutls28-dev libgcrypt-dev libssl-dev libidn11-dev libmongoc-dev libbson-dev libyaml-dev libnghttp2-dev libmicrohttpd-dev libcurl4-gnutls-dev libnghttp2-dev libtins-dev libtalloc-dev meson
+
+# Clone Open5GS
+git clone https://github.com/open5gs/open5gs
+
+# Compile Open5GS
+cd open5gs
+meson build --prefix=`pwd`/install
+ninja -C build
+
+# Configure TUN device
+sudo ./misc/netconf.sh
+
+# Configure the core
+cd ./install/etc/open5gs/
+cp /local/repository/config/open5gs/* .
+sed -i 's/mcc: 901/mcc: 208/g' mme.conf
