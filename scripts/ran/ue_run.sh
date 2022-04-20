29ee43c5f5ec1b041d310ef9c46e230bc5a29600
j0lama <jon_larrea@hotmail.com>
2022-04-20 14:01:56 +0100

    Create a separate branch

--- scripts/ran/ue_run.sh
+++ scripts/ran/ue_run.sh
@@ -10,12 +10,15 @@ if [ ! -f /local/repository/ue-setup-complete ]; then
     exit 0
 fi
 
+# Generate MSIN based on the ID
+MSIN=$(printf "%010d" $1)
+export MSIN
+
 cd /local/repository/openairinterface5g/
 source oaienv
 cd cmake_targets/
 
-# Generate MSIN based on the ID
-MSIN=$(printf "%010d" $1)
+
 
 # Configure SIMs
 cd ran_build/build
@@ -43,18 +46,19 @@ sudo ip netns exec ue$1 ip addr add 1.1.$1.2/24 dev veth$1 # Namespace interface
 
 # Bring interfaces up
 sudo ip link set veth_gw_$1 up
+sudo ip netns exec ue$1 ip link set dev lo up
 sudo ip netns exec ue$1 ip link set veth$1 up
 
 # Enable IPv4 packet forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
 
 FRONTHAUL_IFACE=$(ip route list 192.168.2.3/24 | awk '{print $3}') # Get fronthaul network interface
-# Add packet forwaring rules
-sudo iptables -A FORWARD -o $FRONTHAUL_IFACE -i veth_gw_$1 -j ACCEPT -d 192.168.2.1/24
-sudo iptables -A FORWARD -i $FRONTHAUL_IFACE -o veth_gw_$1 -j ACCEPT -s 192.168.2.1/24
-
 # Create a NAT for the namespace
-sudo iptables -t nat -A POSTROUTING -s 1.1.$1.2/24 -o $FRONTHAUL_IFACE -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 1.1.$1.0/24 -o $FRONTHAUL_IFACE -j MASQUERADE
+
+# Add packet forwaring rules
+sudo iptables -A FORWARD -o $FRONTHAUL_IFACE -i veth_gw_$1 -j ACCEPT
+sudo iptables -A FORWARD -i $FRONTHAUL_IFACE -o veth_gw_$1 -j ACCEPT
 
 # Add default gateway to the namespace
 sudo ip netns exec ue$1 ip route add default via 1.1.$1.1
@@ -62,8 +66,8 @@ sudo ip netns exec ue$1 ip route add default via 1.1.$1.1
 
 # Configure UE configuration file (IP and iface name)
 cp /local/repository/config/ran/ue.conf /local/repository/config/ran/tmp_ue.conf
-VETH_IFACE=$(ip route list 1.1.$1.2/24 | awk '{print $3}')
-sed -i "s/FRONTHAUL_IFACE/$VETH_IFACE/g" /local/repository/config/ran/tmp_ue.conf
+sed -i "s/FRONTHAUL_IFACE/veth$1/g" /local/repository/config/ran/tmp_ue.conf
 sed -i "s/VETH_IP/1.1.$1.2/g" /local/repository/config/ran/tmp_ue.conf
+
 
 sudo -E ./lte-uesoftmodem -O /local/repository/config/ran/tmp_ue.conf --L2-emul 5 --nokrnmod 1 --ue-idx-standalone 2 --num-ues 1 --node-number 2 --log_config.global_log_options level,nocolor,time,thread_id | tee /local/repository/ue.log 2>&1
